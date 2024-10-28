3e011908d0ff40b0adaff3586f57740d973dd6ef
Jamie Cronin <jcronin02@gmail.com>
2024-10-28 21:45:55 +0000

    [Factory Method] Add comments to the UI interface

--- src/factorymethod/ui/ui.go
+++ src/factorymethod/ui/ui.go
@@ -1,11 +1,14 @@
+// Package ui provides an interface for UI elements.
 package ui
 
+// A Button is a clickable button element.
 type Button interface {
 	Render()
 	OnClick(func())
 	Click()
 }
 
+// A Dialog is a container for UI elements.
 type Dialog interface {
 	Render()
 	NewButton() Button

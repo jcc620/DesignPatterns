368f5f470ff9095a0f9ea4c51ad066fc36d0aefa
Jamie Cronin <jcronin02@gmail.com>
2024-11-10 14:04:37 +0000

    [Abstract Factory] Create UI interface

---
+++ src/abstractfactory/ui/ui.go
@@ -0,0 +1,29 @@
+// Package ui provides an interface for UI elements and the abstract UI factory.
+package ui
+
+// A Button is a clickable button element.
+type Button interface {
+	Render()
+	OnClick(func())
+	Click()
+}
+
+// A Checkbox is an element that can be toggled on and off.
+type Checkbox interface {
+	Render()
+	Check()
+	IsChecked() bool
+}
+
+// A Heading is an element to display heading text.
+type Heading interface {
+	Render()
+	SetText(string)
+}
+
+// A Factory is a creator of UI elements.
+type Factory interface {
+	NewButton() Button
+	NewCheckbox() Checkbox
+	NewHeading() Heading
+}
